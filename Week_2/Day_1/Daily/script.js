ca0611a0d96db73da5a8370957ce258be8aa7d23
shovz <shovz12@gmail.com>
2022-06-26 14:12:09 +0300

    Daily ex

---
+++ Week_2/Day_1/Daily/script.js
@@ -0,0 +1,19 @@
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+
+// 1. Remove Banana from the array.
+fruits = fruits.slice(1);
+
+// 2. Sort the array in alphabetical order.
+fruits = fruits.sort();
+
+// 3. Add “Kiwi” to the end of the array.
+fruits.push("Kiwi");
+
+// 4. Remove “Apples” from the array. Don’t use the same method as in part 1.
+
+
+// 5. Sort the array in reverse order. (Not alphabetical, but reverse the current Array i.e. [‘a’, ‘c’, ‘b’] becomes [‘b’, ‘c’, ‘a’])
+// At the end you should see this outcome:
+
+console.log(fruits)
