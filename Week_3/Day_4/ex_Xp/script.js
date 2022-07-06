b03527e259bd25a0dde2f3b179f098742ce15601
shovz <shovz12@gmail.com>
2022-07-07 00:01:55 +0300

    color box done

--- Week_3/Day_4/ex_Xp/script.js
+++ Week_3/Day_4/ex_Xp/script.js
@@ -1,3 +1,4 @@
+let body = document.querySelector("body");
 let mainContainer = document.createElement("div");
 let colorContainer = document.createElement("div");
 let savedColorContainer = document.createElement("div");
@@ -7,12 +8,13 @@ let clearBtn = document.createElement("button");
 let clearText = document.createTextNode("Clear");
 let mouseFlag=0;
 
+// appending elements
 clearBtn.append(clearText);
 colorContainer.append(clearBtn,colorPicker,savedColorContainer);
 mainContainer.append(colorContainer,canvasContainer);
 document.body.append(mainContainer);
 
-
+// assgining clases
 mainContainer.classList.add("mainContainer");
 colorContainer.classList.add("colorContainer");
 savedColorContainer.classList.add("savedColorContainer");
@@ -20,45 +22,64 @@ canvasContainer.classList.add("canvasContainer");
 colorPicker.classList.add("colorPicker");
 clearBtn.classList.add("clearBtn");
 
-
+// assgining attribute
 colorPicker.setAttribute("type","color");
 colorPicker.setAttribute("value","#fff");
 
-const mouseDw = () =>
-{
-    mouseFlag=1;
-   
-}
-const mouseUp = () =>
-{
-    mouseFlag=0;
-}
-document.body.onmousedown = mouseDw;
-document.body.onmouseup  = mouseUp;
+// checking if mouse is still clicking
+const mouseDw = () =>{mouseFlag=true;}
+const mouseUp = () =>{mouseFlag=false;}
+body.addEventListener("mousedown",mouseDw);
+body.addEventListener("mouseup", mouseUp);
 
-const colorPicked = (event)=>{
+
+// get color picker value
+const colorPick = (event)=>{
     let color = document.createElement("div");
     color.classList.add("colorBox");
     color.style.backgroundColor=`${event.target.value}`;
+
+    color.addEventListener("click",()=>{
+        let test = color.style.backgroundColor.split(/\D/);
+        let colorRgb_to_Hex=[];
+        let counter=0;
+        for (let i of test){
+            if(i!=="")
+            {
+                colorRgb_to_Hex[counter]=i;
+                counter++;
+            } 
+        }
+        colorPicker.value = ConvertRGBtoHex(colorRgb_to_Hex[0],
+            colorRgb_to_Hex[1],colorRgb_to_Hex[2]);
+    })
+
     savedColorContainer.append(color);
 }
+// painting once after mouse is pressed
+const paintDown= (event)=>{
+    
+    event.target.style.backgroundColor=`${colorPicker.value}`;
 
-const paint = ()=>{
-    for (let i = 0; i< canvasContainer.childNodes.length;i++)
-    {
-       if(mouseFlag){
-            let temp = canvasContainer.childNodes[i];
-            temp.addEventListener("mouseover",paint2);
-       }
-       else{
-        break;
-       }
+}
+// keep painting until mouse is up
+const paintOver = (event)=>{
+    
+    if(mouseFlag){
+        event.target.style.backgroundColor=`${colorPicker.value}`;
+    }
+
+}
+// checking which box was clicked
+const clicked = ()=>
+{
+    for(let i=0;i<canvasContainer.childNodes.length;i++ ){
+        canvasContainer.childNodes[i].addEventListener("mousedown",paintDown);
+    
+        canvasContainer.childNodes[i].addEventListener("mouseover",paintOver);
     }
 }
-const paint2 = (event)=>{
-    event.target.style.backgroundColor=`${colorPicker.value}`;
-}
-
+//creating Canvas Layout
 const createCanvasDivs =()=>{
     let height= 42;
 	let width= 40;
@@ -66,7 +87,7 @@ const createCanvasDivs =()=>{
     {
         let canvasDiv = document.createElement("div");
         canvasDiv.classList.add("canvasDiv");
-        canvasDiv.addEventListener("mousedown",paint);
+        canvasDiv.addEventListener("click",clicked);
         canvasContainer.append(canvasDiv);
 
     }
@@ -74,18 +95,54 @@ const createCanvasDivs =()=>{
 
 }
 
-for (let i = 0; i< canvasContainer.childNodes.length;i++)
-{
-    console.log("asdsad");
-    canvasContainer.childNodes[i].addEventListener("mousedown",paint);
-    if(mouseFlag===1){
-        canvasContainer.childNodes[i].addEventListener("mouseover",paint);
+
+
+// RGB to hex calc section
+const cases=(exp)=>{
+    console.log(exp);
+    switch(exp)
+    {
+        case 0 :
+           return "0"
+        case 1 :
+           return "1"
+        case 2 :
+           return "2"
+        case 3 :
+           return "3"
+        case 4 :
+           return "4"
+        case 5 :
+           return "5"
+        case 6 :
+           return "6"
+        case 7 :
+           return "7"
+        case 8 :
+           return "8"
+        case 9 :
+           return "9"
+        case 10 :
+           return "A"
+        case 11 :
+           return "B"
+        case 12 :
+           return "C"
+        case 13 :
+           return "D"
+        case 14 :
+           return "E"
+        case 15 :
+           return "F"   
     }
 }
+const ColorToHex = (color) =>{
+    let hexadecimal = cases(Math.floor(color/16)) + cases((color/16 %1) *16);
+    return hexadecimal;
+  } 
+const ConvertRGBtoHex=(red, green, blue)=> {
+    return "#" + ColorToHex(red) + ColorToHex(green) + ColorToHex(blue);
+  }
 
 createCanvasDivs();
-colorPicker.addEventListener("change",colorPicked);
-
-
-// canvasDiv.style.backgroundColor= `${event.target.value}`;
-// canvasContainer
+colorPicker.addEventListener("change",colorPick);
