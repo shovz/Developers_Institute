1ccbcea4b93a5521960d22184a3d170d8412227d
shovz <shovz12@gmail.com>
2022-07-31 15:40:51 +0300

    Week 7 day 1 XP

---
+++ Week_7/Day_1/ex_Xp/main.js
@@ -0,0 +1,15 @@
+// let a1 = 5;
+// let b1 = 10;
+
+// console.log(`Hello you are ${a+b}`);
+
+// Exercise 3 : Nodemon
+let a = 5;
+let b = 10;
+let x;
+let c = 6;
+x = a + b;
+
+console.log(x);
+
+console.log(x+c);
