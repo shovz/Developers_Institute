42ec9ed2cbcfb4798cab25c2c27c6e5800f544cc
shovz <shovz12@gmail.com>
2022-08-01 17:03:47 +0300

    Week 7 Day 2 XP 2

---
+++ Week_7/Day_2/ex_Xp-2/server.js
@@ -0,0 +1,23 @@
+// Exercise 1 : Express & JSON
+let script  = require('./script');
+let express  = require('express');
+
+const app = express();
+
+app.use('/',express.static(__dirname+'/public'));
+
+app.get('/user', (req, res) => {
+    console.log(script.user);
+    res.json(script.user);
+})
+
+app.get('/user/:id',(req,res)=>{
+    const id = req.params;
+    console.log(req.params);
+    res.json(id);
+  })
+
+app.listen(3000);
+
+
+// Exercise 2 : Express & Parameters
